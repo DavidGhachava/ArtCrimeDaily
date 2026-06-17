6dd93e0c18cf6cc21eadb431359d2f03833b24ae
david <daviddavowo@gmail.com>
2026-06-18 02:04:43 +0400

    Add Oreo and Cletice to gallery

--- src/data/galleryItems.js
+++ src/data/galleryItems.js
@@ -1,9 +1,11 @@
 import bigMamaAndBean from '../assets/gallery/big-mama-and-bean.jpg'
 import brutus from '../assets/gallery/brutus.jpg'
+import cletice from '../assets/gallery/cletice.jpg'
 import harvey from '../assets/gallery/harvey.jpg'
 import lafayette from '../assets/gallery/lafayette.jpg'
 import midnight from '../assets/gallery/midnight.jpg'
 import mila from '../assets/gallery/mila.jpg'
+import oreo from '../assets/gallery/oreo.jpg'
 
 export const galleryItems = [
   { name: 'Big Mama and Bean', image: bigMamaAndBean },
@@ -12,4 +14,6 @@ export const galleryItems = [
   { name: 'Lafayette', image: lafayette },
   { name: 'Midnight', image: midnight },
   { name: 'Mila', image: mila },
+  { name: 'Oreo', image: oreo },
+  { name: 'Cletice', image: cletice },
 ]
